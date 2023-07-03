f61661b36f762d9daf951c687a0a075e7d012f3f
Vellshow1 <valek099@bk.ru>
2023-07-03 22:08:07 +0300

    dz3

--- code.java
+++ code.java
@@ -1,21 +1,53 @@
 /**
  * code
  */
-import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
 public class code {
 
     public static void main(String[] args) {
-        try(FileInputStream fin = new FileInputStream("note.txt");
-            FileOutputStream fos = new FileOutputStream("new_file.txt")){
-            byte[] buffer = new byte[256];
-            int i;
-            while ((i = fin.read(buffer)) != -1) {
-                System.out.print((char)i);
-                fos.write(buffer,0,i);                
-            }
-        }
-        catch(IOException ex){
-            System.out.println(ex.getMessage());
-        }
+        
+ 
+
+        List <Integer> list = new ArrayList<Integer>();
+        list.add(11);
+        list.add(2);
+        list.add(5);
+        list.add(7);
+        list.add(8);
+        Collections.sort(list);
+
+        System.out.println(list);
+
+    
+        
+        for (int i=0;i<list.size();i++){
+            int even=list.get(i)%2;
+
+        if (even==0){
+            System.out.println("This is Even Number:"+ list.get(i));
+            list.remove(i);
+
+        // Add this:
+        i--;
     }
+}
+
+        int min = list.get(0);
+        int max = list.get(0);
+        for (Integer i: list) {
+            if(i < min) 
+                min = i;
+            if(i > max) 
+                max = i;
+            }
+
+        System.out.println("минимальное число: " + min);
+        System.out.println("максимальное число: " + max);
+
+        System.out.println(list);
+    
+    }        
 }
--- dz3.java
+++ dz3.java
@@ -1,44 +1,50 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Collections;
 
 public class dz3 {
-    private static int j;
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-                
-
-
-    }
-    static void sortList(){
-
-    List<Integer> numbers = new ArrayList<Integer>();
-
-    numbers.add(11);
-    numbers.add(45);
-    numbers.add(12);
-    numbers.add(32);
-    numbers.add(36);
-
-System.out.println("Unsorted List: " + numbers);
-
-    for (Iterator<Integer> iterator = numbers.iterator(); iterator.hasNext();) {
-        Integer number = iterator.next();
-            if (number % 2 == 0) {
-                System.out.println("This is Even Number: " + number);
-                iterator.remove();
-    }
-
-}
-
-Collections.sort(numbers);
-System.out.println("Sorted List: " + numbers);
-
-
+        
  
 
+        List <Integer> list = new ArrayList<Integer>();
+        list.add(11);
+        list.add(2);
+        list.add(5);
+        list.add(7);
+        list.add(8);
+        Collections.sort(list);
+
+        System.out.println(list);
+
+    
+        
+        for (int i=0;i<list.size();i++){
+            int even=list.get(i)%2;
+
+        if (even==0){
+            System.out.println("This is Even Number:"+ list.get(i));
+            list.remove(i);
+
+        // Add this:
+        i--;
+    }
 }
 
+        int min = list.get(0);
+        int max = list.get(0);
+        for (Integer i: list) {
+            if(i < min) 
+                min = i;
+            if(i > max) 
+                max = i;
+            }
+
+        System.out.println("минимальное число: " + min);
+        System.out.println("максимальное число: " + max);
+
+        System.out.println(list);
+    
+    }        
+}
