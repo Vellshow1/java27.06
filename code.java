35d8c356dbfc0a3c98b730db62d6bd335ab5658b
Vellshow1 <valek099@bk.ru>
2023-07-03 20:52:41 +0300

    dz3

--- code.java
+++ code.java
@@ -5,10 +5,13 @@ import java.io.*;
 public class code {
 
     public static void main(String[] args) {
-        try(FileInputStream fin = new FileInputStream("note.txt")){
+        try(FileInputStream fin = new FileInputStream("note.txt");
+            FileOutputStream fos = new FileOutputStream("new_file.txt")){
+            byte[] buffer = new byte[256];
             int i;
-            while ((i = fin.read()) != -1) {
-                System.out.print((char)i);                
+            while ((i = fin.read(buffer)) != -1) {
+                System.out.print((char)i);
+                fos.write(buffer,0,i);                
             }
         }
         catch(IOException ex){
